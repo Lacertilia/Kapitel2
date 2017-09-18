9866ab6f4b519be7297ff79fa49b8899d6cf89d9
Rasmus Mattas <mara16@nti.umea>
2017-09-18 11:20:45 +0200

    Sista Lektion 2

--- Lektion2.java
+++ Lektion2.java
@@ -27,8 +27,8 @@ public class Lektion2{
         a = "Erik Andersson 860314-2714";
         int d1 = a.indexOf('1');
         int d2 = a.indexOf('-');
-        int m1 = a.indexOf('0');
-        b = a.substring(d1, d2) + "/" + a.substring(m1, d1);
+        int m = a.indexOf('0');
+        b = a.substring(d1, d2) + "/" + a.substring(m, d1);
         JOptionPane.showMessageDialog(null, b);
         }
 }
