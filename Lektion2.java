10e566fd4ece3c0aeab92bb691792a86935b5658
Rasmus Mattas <mara16@nti.umea>
2017-09-18 10:21:08 +0200

    2.3

--- Lektion2.java
+++ Lektion2.java
@@ -2,10 +2,14 @@ import javax.swing.*;
 
 public class Lektion2{
     public static void main(String[] args) {
-        String name = "Rasmus";
+        /*String name = "Rasmus";
         String adress = "Hemma";
         String tfn = "0776969699";
         String info = name + "\n" + adress + "\n" + tfn;
-        JOptionPane.showMessageDialog(null, info);
+        JOptionPane.showMessageDialog(null, info);*/
+        String firstname = JOptionPane.showInputDialog(null, "Ditt f" + '\u00F6' + "rnamn?");
+        String lastname = JOptionPane.showInputDialog(null, "Ditt efternamn?");
+        String fullname = firstname + " " + lastname;
+        JOptionPane.showMessageDialog(null, "Du heter " + fullname + " :)");
     }
 }
